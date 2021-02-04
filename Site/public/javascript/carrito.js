1d7a871a9cfa84eeea0fa21fcea910e5a0132891
Leo Terry <lucasenriquez1997@gmail.com>
2021-02-04 01:12:51 -0300

    retoques de estilo

--- Site/public/javascript/carrito.js
+++ Site/public/javascript/carrito.js
@@ -3,17 +3,29 @@ let carrito = document.querySelector(".carrito")
 let btnCarritoDesktop = document.querySelector(".carrito-desktop")
 let showCarrito = false
 
-let mostrarCarrito = ()=>{
+let mostrarCarritoMobile = ()=>{
     if (showCarrito == false){
         /* carrito.style.display = "inline-block" */
-        carrito.style.left = "0"
+        carrito.style.right = "0"
+        /* carrito.style.left = "70%" */
         showCarrito = true
     } else{
         /* carrito.style.display = "none" */
-        carrito.style.left = "-150%"
+        /* carrito.style.right = "-150%" */
+        carrito.style.right = "-150%"
         showCarrito = false
     }
 }
-btnCarrito.addEventListener("click", mostrarCarrito)
-btnCarritoDesktop.addEventListener("click", mostrarCarrito)
+let mostrarCarritoDesktop = ()=>{
+    if (showCarrito == false){
+        carrito.style.top = "60px"
+        showCarrito = true
+    } else{
+        /* carrito.style.display = "none" */
+        carrito.style.top = "-150%"
+        showCarrito = false
+    }
+}
+btnCarrito.addEventListener("click", mostrarCarritoMobile)
+btnCarritoDesktop.addEventListener("click", mostrarCarritoDesktop)
 
--- Site/public/javascript/nav-menu.js
+++ Site/public/javascript/nav-menu.js
@@ -17,3 +17,46 @@ menuIcon.addEventListener("click", () =>{
     }
    
 })
+
+
+/* Posición del nav */
+
+let navDesktop = document.querySelector(".nav-desktop");
+let actualScroll = 0
+
+window.addEventListener("scroll", () =>{
+    if(window.scrollY < actualScroll && window.scrollY != 0){
+        navDesktop.style.position = "fixed";
+        navDesktop.style.top = "0px";
+        navDesktop.style.marginTop = "0";
+        actualScroll = window.scrollY
+        
+    }else{
+        navDesktop.style.position = "relative";
+        navDesktop.style.top = "";
+        actualScroll = window.scrollY
+    }
+     
+})
+
+let mobileNav = document.querySelector(".mobile");
+let actualScrollM = 0
+
+window.addEventListener("scroll", () =>{
+    if(window.scrollY < actualScrollM && window.scrollY != 0){
+        mobileNav.style.position = "fixed";
+        mobileNav.style.top = "0px";
+        mobileNav.style.marginTop = "0";
+        actualScrollM = window.scrollY
+        
+    }else if (window.scrollY > actualScrollM && window.scrollY != 0){
+        mobileNav.style.position = "relative";
+        mobileNav.style.top = "-100px";
+        actualScrollM = window.scrollY
+    }else{
+        mobileNav.style.position = "relative";
+        mobileNav.style.top = "0";
+        actualScrollM = window.scrollY
+    }
+     
+})
